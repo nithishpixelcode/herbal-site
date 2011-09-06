fe0da57f4b89c3df4a951fc3be0a86450b3d036a
Harshith JV <harshith@pixelcode.ca>
2011-09-06 14:22:39 +0530

    Added remove functionality

--- herbal/urls.py
+++ herbal/urls.py
@@ -15,6 +15,7 @@ urlpatterns = patterns('',
 	(r'^media/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.MEDIA_ROOT}),
 	url(r'^$', 'herbal.views.index', name='index'),
 	url(r'^cart-update/$', 'herbal.views.cart_update', name='cart-update'),
+	url(r'^cart-delete/$', 'herbal.views.cart_delete', name='cart-delete'),
 	url(r'^cart-clear/$', 'herbal.views.cart_clear', name='cart-clear'),
 	url(r'^(?P<nav>[-\w]+)/$', 'herbal.views.nav', name='nav'),
 )
